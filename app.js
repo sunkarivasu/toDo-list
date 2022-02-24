908e493835684684eb6a0c822c8bc014e9561093
sunkari vasu <sunkarivasu1@gmail.com>
2022-02-24 19:25:00 +0530

    version-1

--- app.js
+++ app.js
@@ -1,44 +1,201 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const bodyParser = require("body-parser");
+const { redirect } = require("express/lib/response");
 
 const date = require(__dirname+"/date"); 
 
 const app = express();
 
-var items = []
-
-var workList = []
+mongoose.connect("mongodb+srv://admin-vasu:vasu%40143@cluster0.ypfh3.mongodb.net/ToDoListDB",function(err)
+{
+    if(!err)
+        console.log("connected to database");
+    else
+        console.log("error occured while connecting to database:"+err);
+});
 
 app.set("view engine",'ejs');
-
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+
 app.use(express.static(__dirname+"/public"));
+// app.use(express.json({strict: false}));
+
+
+const itemSchema = new mongoose.Schema({
+    name:String
+});
+
+const listSchema = new mongoose.Schema({
+    name:String,
+    listItems:[itemSchema]
+})
+
+
+
+const Item = mongoose.model("Item",itemSchema);
+
+const List = mongoose.model("List",listSchema);
+
 
 app.get("/",function(req,res)
 {
-    res.render("index",{title:date.getDate(),itemList:items});
+    Item.find(function(err,result)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("index",{title:"Today"/*date.getDate()*/,itemList:result});
+        }
+    })
 });
 
-app.get("/work",function(req,res)
-{
-    res.render("index",{title:"work",itemList:workList})
-});
-
-app.post("/work",function(req,res)
-{
-    var item = req.body.newItem;
-    workList.push(item);
-    res.redirect("/work");
-});
 
 app.post("/",function(req,res)
 {
-    var newItem = req.body.newItem;
-    items.push(newItem);
-    res.redirect("/");
+    const newitem = new Item({
+        name:req.body.newItem
+    }); 
+
+    newitem.save(function(err)
+    {
+        if(!err)
+        {
+            console.log("inserted successfully");
+            res.redirect("/");    
+        }
+        else
+        {
+            console.log("data not inserted");
+        }
+    });
+    
 });
+
+
+app.post("/deleteToday",function(req,res)
+{
+    Item.deleteOne({_id:req.body.number},function(err)
+    {
+        if(!err)
+        {
+            res.redirect("/");
+        }    
+        else
+        {
+            console.log(err);
+        }
+    });
+    
+})
+
+
+
+app.get("/:customListName",function(req,res)
+{
+    var customListName = req.params.customListName;
+    List.findOne({name:customListName},function(err,result)
+    {
+        if(!err)
+        {
+            if(result==undefined)
+                resultItems=[]
+            else
+                resultItems = result.listItems
+            //console.log(resultItems);
+            res.render("index",{title:customListName,itemList:resultItems} );
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
+});
+
+app.post("/:customListName",function(req,res)
+{
+    var customListName =req.params.customListName.slice(6,);
+    console.log(customListName);
+    if(req.params.customListName.slice(0,6)=="delete")
+        {
+            console.log("if block");
+            console.log(req.params.customListName.slice(6,));
+            List.updateOne({name:customListName},{$pull:{listItems:{_id:req.body.number}}},function(err)
+                {
+                    if(err)
+                    {
+                        console.log(err);
+                    }
+                    else
+                    {
+                       console.log("pulled old list");
+                       res.redirect("/"+customListName);
+                    }
+                });
+        }
+    else
+    {
+        console.log("else block")
+        var customListName = req.params.customListName;
+        var newList = req.body.newItem;
+        List.find({name:customListName},function(err,result)
+        {
+            if(!err)
+            {
+                if(result.length==0)
+                {
+                    const newlist = new List(
+                        {
+                            name:customListName,
+                            listItems:[{name:newList}]
+                        })
+                    newlist.save(function(err)
+                    {
+                        if(!err)
+                            res.redirect("/"+customListName);
+                        else
+                            console.log("not inserted");
+
+
+                    });
+                    //console.log("inserted new list");
+                }
+                else
+                {
+                    //console.log("No records found");
+                    List.updateOne({name:customListName},{$push:{listItems:{name:newList}}},function(err)
+                    {
+                        if(err)
+                        {
+                            console.log(err);
+                        }
+                        else
+                        {
+                        // console.log("updated new list");
+                            res.redirect("/"+customListName);
+                        }
+                    });
+                }
+                //console.log("/"+customListName);
+            }
+            else
+            {
+                console.log("error occured"+err);
+            }
+        });
+    }
+});
+
+
+
 
 app.listen("3000",function()
 {
--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,47 @@
 $(document).ready(function()
 {
-    $(".checkbox").change(function()
-    {
-        var number =$(this)[0].classList[1].slice(1,);
-        if($(".item-"+number).hasClass("cross"))
-        {
-            $(".item-"+number).removeClass("cross")
-        }
-        else
-        {
-            $(".item-"+number).addClass("cross") 
-        }
-    })
+    // $(".checkbox").change(function()
+    // {
+    //     var number = $(this)[0].classList[1].slice(1,);
+    //     console.log(number);
+    //     if($(".date").text()=="Today")
+    //     {
+    //         var type="/deleteItem";
+    //     }
+    //     else
+    //     {
+    //         var type="/delete"+$(".date").text();
+    //     }
+    //     $.ajax(
+    //         {
+    //             url:type,
+    //             method:"POST",
+    //             dataType:"json",
+    //             data:{"itemNumber":number},
+    //             success:function(res)
+    //             {
+    //                 if(res.status)
+    //                 {
+    //                     console.log("successfully deleted");
+    //                     $(".item-"+number).addClass("hide");
+    //                 }
+                        
+    //                 else
+    //                     console.log("Not deleted");
+    //             },
+    //             error:function(err)
+    //             {
+    //                 console.log("error occured: "+err);
+    //             }
+    //         });
+    //     // var number =$(this)[0].classList[1].slice(1,);
+    //     // if($(".item-"+number).hasClass("cross"))
+    //     // {
+    //     //     $(".item-"+number).removeClass("cross")
+    //     // }
+    //     // else
+    //     // {
+    //     //     $(".item-"+number).addClass("cross") 
+    //     // }
+    // })
 })
